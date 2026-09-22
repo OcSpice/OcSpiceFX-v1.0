635b61e00df7934e8b0bba293d376a29432afb1f
OGIDIAGBA OGHENEOCHUKO EMMANUEL <ochukoemmanuel05@gmail.com>
2026-09-22 17:38:39 +0100

    Add instrument configuration for historical M5 pipeline

---
+++ data-pipeline/instruments.js
@@ -0,0 +1,15 @@
+export const instruments = [
+  { symbol: 'xauusd', projectSymbol: 'XAUUSD', assetClass: 'commodity' },
+  { symbol: 'gbpusd', projectSymbol: 'GBPUSD', assetClass: 'forex' },
+  { symbol: 'gbpjpy', projectSymbol: 'GBPJPY', assetClass: 'forex' },
+  { symbol: 'usdjpy', projectSymbol: 'USDJPY', assetClass: 'forex' },
+  { symbol: 'eurusd', projectSymbol: 'EURUSD', assetClass: 'forex' },
+  { symbol: 'eurjpy', projectSymbol: 'EURJPY', assetClass: 'forex' },
+  { symbol: 'usa30idxusd', projectSymbol: 'US30', assetClass: 'index' },
+  { symbol: 'dollaridxusd', projectSymbol: 'DXY', assetClass: 'index' },
+  { symbol: 'audjpy', projectSymbol: 'AUDJPY', assetClass: 'forex' },
+  { symbol: 'nzdusd', projectSymbol: 'NZDUSD', assetClass: 'forex' },
+  { symbol: 'nzdjpy', projectSymbol: 'NZDJPY', assetClass: 'forex' }
+];
+export const START_YEAR = 2005;
+export const END_YEAR = new Date().getUTCFullYear();
